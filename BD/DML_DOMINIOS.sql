6376fa82a3d02545640890d09420c0df9294c5f0
Jose Rodriguez <jlrodriguez@asesoftware.com>
2019-02-26 20:12:06 -0500

    Adicionar registro en dominios

--- BD/DML_DOMINIOS.sql
+++ BD/DML_DOMINIOS.sql
@@ -3,4 +3,5 @@
 INSERT INTO DOMINIOS VALUES
 ('TIPO_DOC', 'CC', 'Cédula de Ciudadanía'),
 ('TIPO_DOC', 'CE', 'Cédula de Extranjería'),
-('TIPO_DOC', 'PA', 'Pasaporte');
+('TIPO_DOC', 'PA', 'Pasaporte'),
+('TIPO_DOC', 'TI', 'Tarjeta de Identidad');
