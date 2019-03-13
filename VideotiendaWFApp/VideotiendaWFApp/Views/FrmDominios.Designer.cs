e152b7be29d4ee6ab4612d04e972db8122499e88
jlrodriguezAsw <jlrodriguez@asesoftware.com>
2019-03-12 20:30:55 -0500

    Funcionalidad de Dominios CRUD

---
+++ VideotiendaWFApp/VideotiendaWFApp/Views/FrmDominios.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace VideotiendaWFApp.Views
+{
+    partial class FrmDominios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxFiltro = new System.Windows.Forms.GroupBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.gbxDatos = new System.Windows.Forms.GroupBox();
+            this.grdDominios = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.gbxFiltro.SuspendLayout();
+            this.gbxDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDominios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxFiltro
+            // 
+            this.gbxFiltro.Controls.Add(this.btnLimpiar);
+            this.gbxFiltro.Controls.Add(this.btnBuscar);
+            this.gbxFiltro.Controls.Add(this.txtValor);
+            this.gbxFiltro.Controls.Add(this.txtId);
+            this.gbxFiltro.Controls.Add(this.txtTipo);
+            this.gbxFiltro.Controls.Add(this.lblValor);
+            this.gbxFiltro.Controls.Add(this.lblId);
+            this.gbxFiltro.Controls.Add(this.lblTipo);
+            this.gbxFiltro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gbxFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxFiltro.Location = new System.Drawing.Point(14, 16);
+            this.gbxFiltro.Name = "gbxFiltro";
+            this.gbxFiltro.Size = new System.Drawing.Size(623, 101);
+            this.gbxFiltro.TabIndex = 0;
+            this.gbxFiltro.TabStop = false;
+            this.gbxFiltro.Text = "Filtro de búsqueda";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(455, 60);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(100, 30);
+            this.btnLimpiar.TabIndex = 2;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(334, 60);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 30);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(101, 60);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(200, 21);
+            this.txtValor.TabIndex = 1;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(407, 20);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(200, 21);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.Location = new System.Drawing.Point(101, 20);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(200, 21);
+            this.txtTipo.TabIndex = 1;
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor.Location = new System.Drawing.Point(7, 66);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(88, 15);
+            this.lblValor.TabIndex = 0;
+            this.lblValor.Text = "Valor Dominio:";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(331, 26);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(70, 15);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id Dominio:";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipo.Location = new System.Drawing.Point(11, 26);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(84, 15);
+            this.lblTipo.TabIndex = 0;
+            this.lblTipo.Text = "Tipo Dominio:";
+            // 
+            // gbxDatos
+            // 
+            this.gbxDatos.Controls.Add(this.grdDominios);
+            this.gbxDatos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gbxDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxDatos.Location = new System.Drawing.Point(14, 124);
+            this.gbxDatos.Name = "gbxDatos";
+            this.gbxDatos.Size = new System.Drawing.Size(623, 272);
+            this.gbxDatos.TabIndex = 1;
+            this.gbxDatos.TabStop = false;
+            this.gbxDatos.Text = "Dominios";
+            // 
+            // grdDominios
+            // 
+            this.grdDominios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grdDominios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDominios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grdDominios.Location = new System.Drawing.Point(3, 17);
+            this.grdDominios.MultiSelect = false;
+            this.grdDominios.Name = "grdDominios";
+            this.grdDominios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdDominios.Size = new System.Drawing.Size(617, 252);
+            this.grdDominios.TabIndex = 0;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(534, 402);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 25);
+            this.btnEliminar.TabIndex = 0;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(420, 402);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(100, 25);
+            this.btnEditar.TabIndex = 0;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.Location = new System.Drawing.Point(305, 402);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(100, 25);
+            this.btnNuevo.TabIndex = 0;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.Location = new System.Drawing.Point(16, 402);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(100, 25);
+            this.btnRegresar.TabIndex = 0;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            // 
+            // FrmDominios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 435);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.gbxDatos);
+            this.Controls.Add(this.gbxFiltro);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmDominios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestión  de Dominios";
+            this.Load += new System.EventHandler(this.FrmDominios_Load);
+            this.gbxFiltro.ResumeLayout(false);
+            this.gbxFiltro.PerformLayout();
+            this.gbxDatos.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdDominios)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxFiltro;
+        private System.Windows.Forms.GroupBox gbxDatos;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridView grdDominios;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnRegresar;
+    }
+}
---
+++ VideotiendaWFApp/VideotiendaWFApp/Views/FrmDominios.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideotiendaWFApp.Models;
+
+namespace VideotiendaWFApp.Views
+{
+    public partial class FrmDominios : Form
+    {
+        public FrmDominios()
+        {            
+            InitializeComponent();            
+        }
+
+        private void FrmDominios_Load(object sender, EventArgs e)
+        {
+            refrescarTabla();
+            this.txtTipo.Select();
+        }
+
+        #region Helper
+         public void refrescarTabla()
+        {
+            using (videotiendaEntities db = new videotiendaEntities())
+            {
+                var lstDominios = from d in db.DOMINIOS
+                                  select d;
+                grdDominios.DataSource = lstDominios.ToList();
+            }
+        }
+
+        private DOMINIOS getSelectedItem()
+        {
+            //Inicializar objeto para almacenar dominio seleccionado en la tabla
+            DOMINIOS d = new DOMINIOS();
+            try
+            {
+                //Obtener los valores de la PK del dominio seleccionado en la tabla
+                d.TIPO_DOMINIO = 
+                    grdDominios.Rows[grdDominios.CurrentRow.Index].
+                    Cells[0].Value.ToString();
+                d.ID_DOMINIO =
+                    grdDominios.Rows[grdDominios.CurrentRow.Index].
+                    Cells[1].Value.ToString();
+                //Retornar objeto con datos del dominio seleccionado en la tabla
+                return d;
+            }
+            catch
+            {
+                return null;
+            }            
+        }
+        #endregion
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            using (videotiendaEntities db = new videotiendaEntities())
+            {
+                //Consultar todos los dominios
+                var lstDominios = from d in db.DOMINIOS
+                                  select d;
+
+                //Aplicar filtros dependiendo de lo que el usuario haya ingresado en la pantalla
+                if (!string.IsNullOrEmpty(this.txtTipo.Text))
+                {
+                    lstDominios = lstDominios.Where(d => d.TIPO_DOMINIO.Contains(this.txtTipo.Text));
+                }
+                if (!string.IsNullOrEmpty(this.txtId.Text))
+                {
+                    lstDominios = lstDominios.Where(d => d.ID_DOMINIO.Contains(this.txtId.Text));
+                }
+                if (!string.IsNullOrEmpty(this.txtValor.Text))
+                {
+                    lstDominios = lstDominios.Where(d => d.VLRDOMINIO.Contains(this.txtValor.Text));
+                }
+                //Retornar lista de dominios con los filtros aplicados
+                grdDominios.DataSource = lstDominios.ToList();
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.txtId.Text = "";
+            this.txtTipo.Text = "";
+            this.txtValor.Text = "";
+
+            refrescarTabla();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            Views.FrmGestionarDominios frmGestionarDominios = new Views.FrmGestionarDominios(null,null);
+            frmGestionarDominios.ShowDialog();
+
+            refrescarTabla();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            //Obtener dominio que se seleccionó en la tabla para editar
+            DOMINIOS d = getSelectedItem();
+            //Hubo selección?
+            if(d != null)
+            {
+                //Inicializar formulario de edición de dominios
+                Views.FrmGestionarDominios frmGestionarDominios
+                    = new Views.FrmGestionarDominios(d.TIPO_DOMINIO, d.ID_DOMINIO);
+                //Abrir formulario de edición de dominios
+                frmGestionarDominios.ShowDialog();
+                //Refrescar tabla cuando regrese del formulario de edición
+                refrescarTabla();
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //Obtener el dominio que se va a eliminar
+            DOMINIOS d = this.getSelectedItem();
+            //Hubo selección?
+            if (d != null)
+            {
+                if (MessageBox.Show("Está seguro que desea eliminar este registro?",
+                    "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button1) ==
+                        System.Windows.Forms.DialogResult.Yes)
+                {
+                    //Establecer conexión con la BD a través de EF
+                    using (videotiendaEntities db = new videotiendaEntities())
+                    {
+                        //Buscar el dominio en la BD
+                        DOMINIOS dEliminar = db.DOMINIOS.Find(d.TIPO_DOMINIO, d.ID_DOMINIO);
+                        //Eliminar el dominio de la tabla
+                        db.DOMINIOS.Remove(dEliminar);
+                        //Confirmar cambios en la BD
+                        db.SaveChanges();
+                    }
+                    //Actualizar la tabla de datos
+                    this.refrescarTabla();
+                }
+            }
+        }
+    }
+}
