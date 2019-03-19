69228d899628f284b926857eb275ef7cd5bac854
jlrodriguezl <jrodriguez.51c@gmail.com>
2019-03-19 14:00:50 -0500

    Funcionalidad de Dominios

---
+++ VideotiendaWFApp/VideotiendaWFApp/Views/FrmGestionarDominios.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace VideotiendaWFApp.Views
+{
+    partial class FrmGestionarDominios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlDominios = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pnlDominios.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlDominios
+            // 
+            this.pnlDominios.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlDominios.Controls.Add(this.label4);
+            this.pnlDominios.Controls.Add(this.label3);
+            this.pnlDominios.Controls.Add(this.label2);
+            this.pnlDominios.Controls.Add(this.btnCancelar);
+            this.pnlDominios.Controls.Add(this.btnGuardar);
+            this.pnlDominios.Controls.Add(this.txtValor);
+            this.pnlDominios.Controls.Add(this.txtId);
+            this.pnlDominios.Controls.Add(this.txtTipo);
+            this.pnlDominios.Controls.Add(this.lblValor);
+            this.pnlDominios.Controls.Add(this.lblId);
+            this.pnlDominios.Controls.Add(this.label1);
+            this.pnlDominios.Location = new System.Drawing.Point(16, 12);
+            this.pnlDominios.Name = "pnlDominios";
+            this.pnlDominios.Size = new System.Drawing.Size(359, 161);
+            this.pnlDominios.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tipo Dominio:";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(4, 38);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(70, 15);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id Dominio:";
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor.Location = new System.Drawing.Point(4, 73);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(88, 15);
+            this.lblValor.TabIndex = 0;
+            this.lblValor.Text = "Valor Dominio:";
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.Location = new System.Drawing.Point(112, 4);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(200, 20);
+            this.txtTipo.TabIndex = 1;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(112, 35);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(200, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(112, 68);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(200, 20);
+            this.txtValor.TabIndex = 1;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(54, 115);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(100, 30);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(187, 115);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(100, 30);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(316, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(316, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(316, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "*";
+            // 
+            // FrmGestionarDominios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 182);
+            this.ControlBox = false;
+            this.Controls.Add(this.pnlDominios);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FrmGestionarDominios";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Agregar/Modificar Dominios";
+            this.Load += new System.EventHandler(this.FrmGestionarDominios_Load);
+            this.pnlDominios.ResumeLayout(false);
+            this.pnlDominios.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlDominios;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ VideotiendaWFApp/VideotiendaWFApp/Views/FrmGestionarDominios.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideotiendaWFApp.Models;
+
+namespace VideotiendaWFApp.Views
+{
+    public partial class FrmGestionarDominios : Form
+    {
+        DOMINIOS oDominio = null;
+        private String tipoDominio;
+        private String IdDominio;
+
+        public FrmGestionarDominios(String tipoDominio, String idDominio)
+        {
+            //Dibujar el formulario
+            InitializeComponent();
+            //Recibir los datos de la PK (si son nulos estamos insertando. 
+            //Si hay datos estamos editando)
+            this.tipoDominio = tipoDominio;
+            this.IdDominio = idDominio;
+            
+            //Si hay datos (edición), llamamos a cargarDatos()
+            if(!string.IsNullOrEmpty(this.IdDominio) &&
+                !string.IsNullOrEmpty(this.tipoDominio))
+            {
+                //Si es modo Edición, bloqueamos los TextBox de la PK
+                cargarDatos();
+                this.txtTipo.ReadOnly = true;
+                this.txtId.ReadOnly = true;
+            }
+            else
+            {
+                //Si es modo Inserción habilitamos los TextBox de la PK
+                this.txtTipo.ReadOnly = false;
+                this.txtId.ReadOnly = false;
+            }
+        }
+
+        private void cargarDatos()
+        {
+            using (videotiendaEntities db = new videotiendaEntities())
+            {
+                oDominio = db.DOMINIOS.Find(tipoDominio, IdDominio);
+                txtTipo.Text = oDominio.TIPO_DOMINIO;
+                txtId.Text = oDominio.ID_DOMINIO;
+                txtValor.Text = oDominio.VLRDOMINIO;
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //Validar que todos los campos obligatorios se hayan diligenciado
+            if (string.IsNullOrEmpty(this.txtId.Text) || string.IsNullOrEmpty(this.txtTipo.Text) || string.IsNullOrEmpty(this.txtValor.Text)){
+                MessageBox.Show("Los campos marcados con (*) son obligatorios");
+            }
+            else
+            {
+                //Establecer conexión con la BD a través de EF
+                using(videotiendaEntities db = new videotiendaEntities())
+                {
+                    //Si estamos en modo Inserción, inicializamos el objeto oDominio
+                    if(this.tipoDominio == null && this.IdDominio == null)                    
+                        oDominio = new DOMINIOS();
+                    
+                    oDominio.TIPO_DOMINIO = this.txtTipo.Text;
+                    oDominio.ID_DOMINIO = this.txtId.Text;
+                    oDominio.VLRDOMINIO = this.txtValor.Text;
+                    //En modo inserción, adicionamos un nuevo registro
+                    if (this.tipoDominio == null && this.IdDominio == null)
+                        db.DOMINIOS.Add(oDominio);
+                    else
+                        //En modo edición, cambiamos el estado del objeto a Modificación
+                        db.Entry(oDominio).State = System.Data.Entity.EntityState.Modified;
+                    //Confirmar cambios en la BD
+                    db.SaveChanges();
+                    //Cerrar el formulario y volver al formulario de datos
+                    this.Close();
+                }
+            }
+        }
+
+        private void FrmGestionarDominios_Load(object sender, EventArgs e)
+        {
+            this.txtTipo.Select();
+        }
+    }
+}
