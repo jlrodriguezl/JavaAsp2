49a28de71e67721c7d16b3aa65b21d8be6a055df
Andres Cifuentes <cifuentes.andres1707@gmail.com>
2019-03-19 20:45:38 -0500

    Nuevo formulario Categorias

---
+++ VideotiendaWFApp/VideotiendaWFApp/Views/FrmCategoria.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace VideotiendaAPP.Views
+{
+    partial class FrmCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxFiltro = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblcategodia = new System.Windows.Forms.Label();
+            this.txtncategoria = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.grdDatos = new System.Windows.Forms.DataGridView();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.gbxFiltro.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxFiltro
+            // 
+            this.gbxFiltro.Controls.Add(this.btnLimpiar);
+            this.gbxFiltro.Controls.Add(this.btnBuscar);
+            this.gbxFiltro.Controls.Add(this.txtncategoria);
+            this.gbxFiltro.Controls.Add(this.lblcategodia);
+            this.gbxFiltro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gbxFiltro.Location = new System.Drawing.Point(59, 39);
+            this.gbxFiltro.Name = "gbxFiltro";
+            this.gbxFiltro.Size = new System.Drawing.Size(938, 95);
+            this.gbxFiltro.TabIndex = 0;
+            this.gbxFiltro.TabStop = false;
+            this.gbxFiltro.Text = "Filtro de Busqueda";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.grdDatos);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox2.Location = new System.Drawing.Point(59, 140);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(938, 273);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Categorias";
+            // 
+            // lblcategodia
+            // 
+            this.lblcategodia.AutoSize = true;
+            this.lblcategodia.Location = new System.Drawing.Point(40, 26);
+            this.lblcategodia.Name = "lblcategodia";
+            this.lblcategodia.Size = new System.Drawing.Size(124, 15);
+            this.lblcategodia.TabIndex = 0;
+            this.lblcategodia.Text = "Nombre Categoria";
+            // 
+            // txtncategoria
+            // 
+            this.txtncategoria.Location = new System.Drawing.Point(219, 23);
+            this.txtncategoria.Name = "txtncategoria";
+            this.txtncategoria.Size = new System.Drawing.Size(200, 21);
+            this.txtncategoria.TabIndex = 1;
+            this.txtncategoria.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtncategoria.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(485, 20);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 30);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(660, 20);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(100, 30);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // grdDatos
+            // 
+            this.grdDatos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grdDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDatos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grdDatos.Location = new System.Drawing.Point(3, 17);
+            this.grdDatos.Name = "grdDatos";
+            this.grdDatos.Size = new System.Drawing.Size(932, 253);
+            this.grdDatos.TabIndex = 0;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(62, 445);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(100, 30);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(296, 445);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(100, 30);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(544, 445);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 30);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Location = new System.Drawing.Point(824, 449);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(100, 30);
+            this.btnRegresar.TabIndex = 5;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // FrmCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 519);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.gbxFiltro);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FrmCategoria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Gestion de Categoria";
+            this.Load += new System.EventHandler(this.FrmCategoria_Load);
+            this.gbxFiltro.ResumeLayout(false);
+            this.gbxFiltro.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdDatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxFiltro;
+        private System.Windows.Forms.TextBox txtncategoria;
+        private System.Windows.Forms.Label lblcategodia;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridView grdDatos;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnRegresar;
+    }
+}
---
+++ VideotiendaWFApp/VideotiendaWFApp/Views/FrmCategoria.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideotiendaAPP.Models;  // carga la app con el modelo categoria
+
+
+namespace VideotiendaAPP.Views
+{
+    public partial class FrmCategoria : Form
+    {
+        public FrmCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmDominios_Load(object sender, EventArgs e)
+        {
+            refrescarTabla(); // cargara lpos datos cuanfdo inicia
+        }
+
+
+        #region Helper
+
+        public void refrescarTabla()
+        {
+            using (videotiendaEntities db = new videotiendaEntities())
+            {
+                var ltsCategoria = from d in db.CATEGORIAS
+                                   select d;
+                // datasource = a consultar a base de datos
+                grdDatos.DataSource = ltsCategoria.ToList(); // se define para que pueda traer la lts de la base de datos
+
+            }
+        }
+
+        private CATEGORIAS getselectedItem()
+        {
+            // inicializa objeto para almacenar dominios seleccionado en la tabla
+
+            CATEGORIAS d = new CATEGORIAS();
+
+            try
+            {
+                // va la a fila categoria y toma el indice   rows =filas   cells = celda
+
+                d.NOM_CATEGORIA = grdDatos.Rows[grdDatos.CurrentRow.Index].Cells[0].Value.ToString();
+
+                return d;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        #endregion 
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            // primer paso conetar base de datso video tienda  
+            using (videotiendaEntities db = new videotiendaEntities())
+            {
+
+                //segundo consultar los categorias
+
+                var ltsCategoria = from d in db.CATEGORIAS select d;
+                // var definimos lstCategorua
+
+                // aplicar fltros ingresados por el usuario
+
+                //aplicar busqueda en nombre categoria
+                if (!String.IsNullOrEmpty(this.txtncategoria.Text)) // VALIDA SI ESTA VACIO O NULL
+                {
+                    ltsCategoria = ltsCategoria.Where(d => d.NOM_CATEGORIA.Contains(this.txtncategoria.Text));
+                }
+
+                grdDatos.DataSource = ltsCategoria.ToList();
+                // grADatos es para que cargue la tabla en gried view
+            }
+        }
+
+        // BORRAR LO QUE SE HACE EN LOS TXTB
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            //SE DIRIJE A LOS TXBOX Y LOS LIMPIA 
+            this.txtncategoria.Text = " ";
+
+            refrescarTabla();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmCategoria_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+   
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
