fd48eb40f8d78ca4059530c78a1b8d40e726871c
JCortes87 <jcortesh1987@gmail.com>
2019-03-20 23:05:26 -0500

    Implementación de productores

--- VideotiendaWFApp/VideotiendaWFApp/Views/FrmGestionarProductores.cs
+++ VideotiendaWFApp/VideotiendaWFApp/Views/FrmGestionarProductores.cs
@@ -7,16 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VideotiendaWFApp.Models;
 
 namespace VideotiendaWFApp.Views
 {
     public partial class FrmGestionarProductores : Form
     {
-        public FrmGestionarProductores()
+        PRODUCTORES oProductores = null;
+        private string idProductor;
+        private string nomProductor;
+            
+
+        public FrmGestionarProductores(string idProductor, string nomProductor)
         {
             InitializeComponent();
-        }
 
+            this.idProductor = idProductor;
+            this.nomProductor = nomProductor;
+
+            if (!string.IsNullOrEmpty(this.idProductor) && !string.IsNullOrEmpty(this.nomProductor))
+            {
+                cargarDatos();
+            }
+        }
+        private void cargarDatos()
+        {
+            using (videotiendaEntities db = new videotiendaEntities())
+            {
+                oProductores = db.PRODUCTORES.Find(idProductor, nomProductor);
+                txtTipo.Text = oProductores.NOM_PROD;
+            }
+        }
         private void FrmGestionarProductores_Load(object sender, EventArgs e)
         {
 
